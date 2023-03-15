9a3df90a371513e8169941c9ef75ac13c8853822
Ilja van Ipenburg <ivanipenburg@gmail.com>
2023-03-15 15:52:35 +0100

    added amplify backend to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import { Amplify } from 'aws-amplify'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import awsExports from './aws-exports'
 import './index.css'
+
+Amplify.configure(awsExports)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
