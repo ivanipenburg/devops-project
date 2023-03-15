1fb15b23cfcb0826df004bc7ddb07768b9af32d1
Ilja van Ipenburg <ivanipenburg@gmail.com>
2023-03-15 12:16:13 +0100

    added pages

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import NavBar from '../components/NavBar'
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/pages/Rooms.js
@@ -0,0 +1,12 @@
+import NavBar from '../components/NavBar'
+
+const Rooms = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Rooms</h1>
+    </div>
+  )
+}
+
+export default Rooms
