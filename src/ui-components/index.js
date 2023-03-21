4c0b313142ca74210b98e6983f83b3c979a3c18a
wolneberg <ingrid_marie@wolneberg.no>
2023-03-21 20:38:34 +0100

    lint er irr

--- src/ui-components/index.js
+++ src/ui-components/index.js
@@ -4,6 +4,6 @@
  * Any changes to this file will be overwritten when running amplify pull. *
  **************************************************************************/
 
-export { default as studioTheme } from './studioTheme';
-export { default as PrivateRoomCreateForm } from './PrivateRoomCreateForm';
-export { default as PrivateRoomUpdateForm } from './PrivateRoomUpdateForm';
+export { default as studioTheme } from './studioTheme'
+export { default as PrivateRoomCreateForm } from './PrivateRoomCreateForm'
+export { default as PrivateRoomUpdateForm } from './PrivateRoomUpdateForm'
