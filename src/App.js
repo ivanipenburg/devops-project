3d6ddbd2c51b5566c2873074ed21b3002cec4bd8
wolneberg <ingrid_marie@wolneberg.no>
2023-03-22 15:41:42 +0100

    frontend test setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
 import Admin from './pages/Admin'
 import Home from './pages/Home'
 import Household from './pages/Household'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
 import Rooms from './pages/Rooms'
+import './App.css'
 
 import '@aws-amplify/ui-react/styles.css'
 
---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import '../__mocks__/mockJsdom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+it('renders without crashing', () => {
+  
+});
+/** test routing in App.js */
